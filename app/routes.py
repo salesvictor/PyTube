c377a3eb4a592e8f65e0a0a8e17efabf42f9513c
Victor Sales <salesrvictor@gmail.com>
2018-05-27 15:52:06 -0300

    minimum application for Flask

---
+++ app/routes.py
@@ -0,0 +1,11 @@
+from flask import render_template
+from app import app
+
+@app.route('/')
+@app.route('/index')
+def index():
+  return render_template('index.html', videos=range(13))
+
+@app.route('/404')
+def error404():
+  return render_template('404.html')
