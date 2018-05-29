0c5c8b742f550d59ff193257f6ad3bec2f4aa6bc
Diego Teixeira Barreto Lima <diegoblima2010@gmail.com>
2018-05-29 11:43:48 -0300

    navbar fixed on top

--- app/static/script.js
+++ app/static/script.js
@@ -1,1 +1,10 @@
 // main js script file
+function expand() {
+  var header = document.getElementById("navbarHeader")
+  var div = document.getElementById("expand")
+  if (header.classList.contains('show')) {
+    div.style.paddingTop = '0'
+  } else {
+    div.style.paddingTop = '13rem'
+  }
+}
