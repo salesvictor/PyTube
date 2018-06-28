33167933ddf8506858e9d3bbc5e5f93c6c0700f6
Diego Teixeira Barreto Lima <diegoblima2010@gmail.com>
2018-06-27 22:39:08 -0300

    Video uploading page design improved

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template, request
 from app import app
-# from app import db
-# from app.models import Video
+from app import db
+from app.models import Video
 
 user = None # not authenticated forced
 
@@ -10,17 +10,17 @@ user = None # not authenticated forced
 def index():
   return render_template('index.html', videos=range(13), user=user)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
   return render_template('login.html')
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
   if request.method == 'POST':
-    # video_file = request.files['video_file']
-    # video = Video(user=user, name=video_file.filename, data=video_file.read())
-    # db.session.add(video)
-    # db.session.commit()
+    video_file = request.files['video_file']
+    video = Video(user=user, name=video_file.filename, data=video_file.read())
+    db.session.add(video)
+    db.session.commit()
     return redirect(url_for('index'))
 
   return render_template('upload.html')
