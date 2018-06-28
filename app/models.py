6378e1678df9cd715bc6fb1601ba83fc2a7c023b
Victor Sales <salesrvictor@gmail.com>
2018-06-28 06:14:14 -0300

    before error

--- app/models.py
+++ app/models.py
@@ -9,6 +9,7 @@ class User(UserMixin, db.Model):
   email = db.Column(db.String(120), index=True, unique=True)
   password_hash = db.Column(db.String(128))
   videos = db.relationship('Video', backref='author', lazy='dynamic')
+  posts = db.relationship('Post', backref='author', lazy='dynamic')
  
   def set_password(self, password):
     self.password_hash = generate_password_hash(password)
@@ -26,6 +27,8 @@ class Video(db.Model):
   title = db.Column(db.String(140))
   description = db.Column(db.String(600), default="")
   timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+  views = db.Column(db.Integer, default=0)
+  posts = db.relationship('Post', backref='video', lazy='dynamic')
   user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
   def __repr__(self):
@@ -34,3 +37,15 @@ class Video(db.Model):
 @login.user_loader
 def load_user(id):
   return User.query.get(int(id))
+
+class Post(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  body = db.Column(db.String(160))
+  timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+  user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+  video_id = db.Column(db.Integer, db.ForeignKey('video.id'))
+
+class Watched(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  user_id = db.Column(db.Integer)
+  video_id = db.Column(db.Integer)
